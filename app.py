a81c68daa73f09856823948f82344a28867fa15f
Olha Korotkina <olhakorotkina@gmail.com>
2024-02-29 16:27:09 +0000

    Added division support

--- app.py
+++ app.py
@@ -8,17 +8,23 @@ def subtract(x, y):
     return x - y
 
 
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
+print("4.Divide")
 
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2): ")
+    choice = input("Enter choice(1/2/4): ")
 
     # check if choice is one of the options
-    if choice in ('1', '2'):
+    if choice in ('1', '2', '4'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -31,6 +37,9 @@ while True:
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
 
         # check if user wants another calculation
         # break the while loop if answer is no
